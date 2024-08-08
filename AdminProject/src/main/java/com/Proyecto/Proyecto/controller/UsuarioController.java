8f5465338e8462c57af4827f22f06d148717a01f
Tohdz <hhernandez70088@ufide.ac.cr>
2024-08-08 11:50:28 -0600

    bugs
    hhernan

--- AdminProject/src/main/java/com/Proyecto/Proyecto/controller/UsuarioController.java
+++ AdminProject/src/main/java/com/Proyecto/Proyecto/controller/UsuarioController.java
@@ -1,5 +1,6 @@
-
 package com.Proyecto.Proyecto.controller;
+
+import com.Proyecto.Proyecto.Domain.Sedes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +11,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import com.Proyecto.Proyecto.Domain.Usuario;
 import com.Proyecto.Proyecto.Service.UsuarioService;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
-    
-     @Autowired
+
+    @Autowired
     private UsuarioService usuarioService;
 
     @GetMapping("/listado")
@@ -25,7 +29,10 @@ public class UsuarioController {
         var sedes = usuarioService.getSedes();
         model.addAttribute("sedes", sedes);
         model.addAttribute("usuarios", usuarios);
-        model.addAttribute("totalUsuarios", usuarios.size());
+        List<Sedes> sedes2 = usuarioService.getSedes();
+        Map<Long, String> sedesMap = sedes2.stream()
+                .collect(Collectors.toMap(Sedes::getIdSede, Sedes::getNombre));
+        model.addAttribute("sedesMap", sedesMap);
         return "/usuario/listado";
     }
 
@@ -56,12 +63,12 @@ public class UsuarioController {
         model.addAttribute("sedes", sedes);
         return "/usuario/modifica";
     }
-    
+
     @PostMapping("/modificar2")
-    public String usuarioModificar2(@RequestParam("idUsuario")Long idUsuario, @RequestParam("username") String username,@RequestParam("password") String password, @RequestParam("nombre") String nombre,
-            @RequestParam("apellidos")String apellidos, @RequestParam("correo") String correo,@RequestParam("telefono") String telefono,@RequestParam("idSede")Long idSede, @RequestParam(value = "estado", defaultValue = "false") boolean estado) {
+    public String usuarioModificar2(@RequestParam("idUsuario") Long idUsuario, @RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("nombre") String nombre,
+            @RequestParam("apellidos") String apellidos, @RequestParam("correo") String correo, @RequestParam("telefono") String telefono, @RequestParam("idSede") Long idSede, @RequestParam(value = "estado", defaultValue = "false") boolean estado) {
         var codigo = new BCryptPasswordEncoder();
-        usuarioService.updateuser(idUsuario,username,codigo.encode(password),nombre,apellidos,correo,telefono,idSede,estado);
+        usuarioService.updateuser(idUsuario, username, codigo.encode(password), nombre, apellidos, correo, telefono, idSede, estado);
         return "redirect:/usuario/listado";
     }
 }
